0011bdc80937da90814b8fdde92e235ee486d38b
Jessica Pope <jessica.pope.co@gmail.com>
2019-05-29 11:43:15 -0600

    added the link_tos to the welcome page

---
+++ spec/dog_spec.rb
@@ -0,0 +1,9 @@
+require_relative "../app/models/dog"
+
+RSpec.describe Dog, "#go_to_school" do
+  context "given a new student" do
+    it "should have lots of energy" do
+      expect(true).to be true
+    end
+  end
+end
